86b138a98928c8db0aaef622fb73d48ff2fd7068
juditzentai1993 <juditzentai1993@gmail.com>
2020-10-10 12:56:33 -0700

    Create new Person class, make some action with it

---
+++ CreateClass/CreateClass/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateClass
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        public enum Genders
+        {
+            Male,
+            Female
+        }
+
+        public override string ToString()
+        {
+            return "A person has a name, birthdate and a gender.";
+        }
+    }
+}
---
+++ CreateClass/CreateClass/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Dude!");
+
+            Person susan = new Person{Name = "Susan", BirthDate = DateTime.Parse("1990-01-25")};
+
+            Console.WriteLine(susan.ToString());
+            Console.WriteLine(susan.Name);
+            Console.WriteLine(susan.BirthDate);
+
+            Console.ReadLine();
+
+        }
+    }
+}
