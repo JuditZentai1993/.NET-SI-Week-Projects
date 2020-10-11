6e2128b9b70bff0e07e822d921a90e18cf2b5238
juditzentai1993 <juditzentai1993@gmail.com>
2020-10-11 09:52:34 -0700

    Finish 'Working with strings' exercise

---
+++ SortString/SortString/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string test = "en vagyok a tuzok";
+            string[] words = test.Split(' ');
+            Array.Sort(words);
+            string testReWritten = string.Join(" ", words);
+            Console.WriteLine(testReWritten);
+            Console.ReadLine();
+        }
+    }
+}
