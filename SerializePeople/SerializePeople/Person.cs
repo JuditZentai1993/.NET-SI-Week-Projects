a04dda5aed7e75d335bfbcd7023375da4f71b40a
juditzentai1993 <juditzentai1993@gmail.com>
2020-10-24 07:41:44 -0700

    Create SerializePeople project, add Person class, extend Person class with Age property

---
+++ SerializePeople/SerializePeople/Person.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializePeople
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        public enum Genders
+        {
+            Male,
+            Female
+        }
+
+        public int Age()
+        {
+            int age = DateTime.Today.Year - BirthDate.Year;
+            if (BirthDate.Month > DateTime.Today.Month)
+            {
+                return age-1;
+            }
+            else if (BirthDate.Month == DateTime.Today.Month)
+            {
+                if (BirthDate.Day > DateTime.Today.Day)
+                {
+                    return age-1;
+                }
+                else
+                {
+                    return age;
+                }
+            }
+            else
+            {
+                return age;
+            }
+        }
+
+        public override string ToString()
+        {
+            return "A person has a name, birthdate and a gender.";
+        }
+    }
+}
---
+++ SerializePeople/SerializePeople/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializePeople
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person susan = new Person { Name = "Susan", BirthDate = DateTime.Parse("1990-10-25") };
+
+            Console.WriteLine(susan.ToString());
+            Console.WriteLine(susan.Name);
+            Console.WriteLine(susan.BirthDate);
+            Console.WriteLine(susan.Age());
+            Console.ReadKey();
+        }
+    }
+}
