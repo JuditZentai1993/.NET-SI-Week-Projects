e7faf1350c64a994c341d8ad3e5c31ee949527e3
juditzentai1993 <juditzentai1993@gmail.com>
2020-11-06 00:54:59 +0100

    Show Menu items when Menu is clicked

--- reactomon/src/components/navbar.jsx
+++ reactomon/src/components/navbar.jsx
@@ -2,11 +2,27 @@ import React, { Component } from "react";
 import "./navbar.css";
  
 class NavBar extends Component {
+  showMenuItems() {
+    const menuItems = document.querySelector(".menu-items");
+    if (!menuItems.classList.contains("clicked")){
+      menuItems.classList.replace("hide-menu-items", "show-menu-items");
+      menuItems.classList.add("clicked");
+    }
+    else {
+      menuItems.classList.replace("show-menu-items", "hide-menu-items");
+      menuItems.classList.remove("clicked");
+    }
+
+  }
   render() {
     return (
       <React.Fragment>
-        <nav className="navbar">
-            <span>Menu</span>
+        <nav className="navbar" onClick={this.showMenuItems}>
+            <div>Menu</div>
+            <div className="menu-items hide-menu-items">
+              <div>Show me Pokemons!</div>
+              <div>Show me Pokemon Types!</div>
+            </div>
         </nav>
       </React.Fragment>
     );
