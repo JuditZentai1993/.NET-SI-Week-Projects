233fcae08bc86e10f93144049d45a6b834453621
juditzentai1993 <juditzentai1993@gmail.com>
2020-11-06 00:29:03 +0100

    Create welcome text in h1 tags and menu (doesn't do anything yet)

--- reactomon/src/components/navbar.jsx
+++ reactomon/src/components/navbar.jsx
@@ -1,17 +1,15 @@
 import React, { Component } from "react";
+import "./navbar.css";
  
 class NavBar extends Component {
   render() {
     return (
       <React.Fragment>
-        <nav className="navbar navbar-dark bg-dark mb-3">
-          <a className="navbar-brand" href="#">
-            <h1>Total Items <span className="badge badge-secondary">{this.props.totalItems}</span></h1>
-          </a>
+        <nav className="navbar">
+            <span>Menu</span>
         </nav>
       </React.Fragment>
     );
-  }
-}
+  }}
  
 export default NavBar;
