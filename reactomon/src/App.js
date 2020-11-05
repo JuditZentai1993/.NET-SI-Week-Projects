e384a24d70215ec1747b8c4c9db88e7ecde56cbb
juditzentai1993 <juditzentai1993@gmail.com>
2020-11-05 01:15:28 +0100

    Solve git merge issues

--- reactomon/src/App.js
+++ reactomon/src/App.js
@@ -1,25 +1,74 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from "react";
+import NavBar from "./components/navbar";
+import Items from "./components/items";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+class App extends Component {
+  state = {
+    items: [{ id: 1, value: 0 }, { id: 2, value: 0 }, { id: 3, value: 0 }]
+  };
+ 
+  handleIncrement = item => {
+    const items = [...this.state.items];
+    const index = items.indexOf(item);
+    items[index] = { ...item };
+    items[index].value++;
+    this.setState({ items });
+  };
+ 
+  handleReset = () => {
+    const items = this.state.items.map(i => {
+      i.value = 0;
+      return i;
+    });
+    this.setState({ items });
+  };
+ 
+  handleDelete = itemId => {
+    const items = this.state.items.filter(item => item.id !== itemId);
+    this.setState({ items: items });
+  };
+ 
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar
+          totalItems={this.state.items.reduce((prev, cur) => {
+            return prev + cur.value;
+          }, 0)}
+        />
+        <Items
+          onReset={this.handleReset}
+          onDelete={this.handleDelete}
+          onIncrement={this.handleIncrement}
+          items={this.state.items}
+        />
+      </React.Fragment>
+    );
+  }
 }
-
+ 
 export default App;
