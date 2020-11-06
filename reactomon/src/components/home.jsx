38bb88abbc8b75c4b7040ed11e410a302df6be9c
juditzentai1993 <juditzentai1993@gmail.com>
2020-11-06 01:58:12 +0100

    Create Home component

---
+++ reactomon/src/components/home.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+// import "./navbar.css";
+// import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+ 
+class Home extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <div>Hello home!</div>
+      </React.Fragment>
+    );
+  }}
+ 
+export default Home;
