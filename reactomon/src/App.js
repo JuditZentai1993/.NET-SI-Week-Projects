639e91449149faa0c58988387883ff801dd46861
juditzentai1993 <juditzentai1993@gmail.com>
2020-11-06 01:58:00 +0100

    Create routes for Home, Pokemons and Types

--- reactomon/src/App.js
+++ reactomon/src/App.js
@@ -1,18 +1,30 @@
 import './App.css';
 import React, { Component } from "react";
 import NavBar from "./components/navbar";
+import Home from './components/home';
+import Pokemons from './components/pokemons';
+import Types from './components/types';
 import "./App.css";
+import {BrowserRouter as Router, NavLink, Route, Switch} from 'react-router-dom';
 
-class App extends Component {
-  render() {
+function App() {
     return (
-      <React.Fragment>
-        <h1> Welcome to Reactomon!</h1>
-        <NavBar
-        />
-      </React.Fragment>
+      <Router>
+        <div>
+          
+          <NavBar/>
+
+            <Switch>
+
+              <Route exact path="/" component={Home}/>
+              <Route path="/pokemons" component={Pokemons}/>
+              <Route path="/types" component={Types}/>
+
+            </Switch>
+
+        </div>
+      </Router>
     );
   }
-}
- 
+
 export default App;
