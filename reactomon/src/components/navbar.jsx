263d06cab65bdedf61f4cf7fb947f420cecf11a2
juditzentai1993 <juditzentai1993@gmail.com>
2020-11-06 01:57:12 +0100

    Refactor Navbar, take Menu out and show Menu Items straight away

--- reactomon/src/components/navbar.jsx
+++ reactomon/src/components/navbar.jsx
@@ -1,36 +1,21 @@
 import React, { Component } from "react";
 import "./navbar.css";
-import Pokemons from "./pokemons";
-import {BrowserRouter as Router, NavLink, Route, Switch} from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
  
 class NavBar extends Component {
-  showMenuItems() {
-    const menuItems = document.querySelector(".menu-items");
-    if (!menuItems.classList.contains("clicked")){
-      menuItems.classList.replace("hide-menu-items", "show-menu-items");
-      menuItems.classList.add("clicked");
-    }
-    else {
-      menuItems.classList.replace("show-menu-items", "hide-menu-items");
-      menuItems.classList.remove("clicked");
-    }
-
-  }
   render() {
     return (
-      <React.Fragment>
-        <Router>
-          <nav className="navbar" onClick={this.showMenuItems}>
-              <div>Menu</div>
-          </nav>
-          <nav className="navbar menu-items hide-menu-items">
-            <Switch>
-                <div><NavLink to="/pokemons"> Pokemons </NavLink></div>
-                <Route path="/pokemons" component={Pokemons}/>
-            </Switch>
-          </nav>
-        </Router>
-      </React.Fragment>
+      <div className="Nav">
+        <nav>
+          <ul className="nav-links">
+
+            <li><NavLink to="/"> Home </NavLink></li>
+            <li><NavLink to="/pokemons"> Pokemons </NavLink></li>
+            <li><NavLink to="/types"> Types </NavLink></li>
+
+          </ul>
+        </nav>
+      </div>
     );
   }}
  
