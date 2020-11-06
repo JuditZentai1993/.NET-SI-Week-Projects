c6a6a8fd4711e84699eb6ad2e67f4df5cb9cd14f
juditzentai1993 <juditzentai1993@gmail.com>
2020-11-06 01:58:23 +0100

    Create Types component

---
+++ reactomon/src/components/types.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+// import "./navbar.css";
+// import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+ 
+class Types extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <div>Hello types!</div>
+      </React.Fragment>
+    );
+  }}
+ 
+export default Types;
