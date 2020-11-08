ed78f2a22912beb6e8c91306a63692e68775fd5c
juditzentai1993 <juditzentai1993@gmail.com>
2020-11-08 19:49:39 +0100

    List pokemon details when user clicks on a pokemon name

--- reactomon/src/components/pokemon_details.jsx
+++ reactomon/src/components/pokemon_details.jsx
@@ -1,33 +1,63 @@
 import React, { Component } from "react";
 import axios from 'axios';
-// import "./navbar.css";
-// import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
  
 export default class PokemonDetails extends Component {
-    // state = {
-    //     pokemons: []
-    // }
 
-    // componentDidMount() {
-    //     axios.get(`https://pokeapi.co/api/v2/pokemon?offset=0&limit=1100`)
-    //       .then(res => {
-    //         const pokemons = res.data.results;
-    //         console.log(this.state.pokemons);
-    //         console.log(pokemons.results);
-    //         this.setState({ pokemons });
-    //       })
-    //   }
+    state = {
+        abilities: [],
+        baseExperience: Number,
+        height: Number,
+        weight: Number,
+        name: String
+    }
+
+    componentDidMount() {
+        let id = this.props.match.params.id;
+        axios.get('https://pokeapi.co/api/v2/pokemon/'+ id)
+          .then(res => {
+            const abilities = res.data.abilities.map( ability => ability.ability.name );
+            console.log(res.data);
+            this.setState({ abilities });
+            const baseExperience = res.data.base_experience;
+            this.setState({ baseExperience });
+            const height = res.data.height;
+            this.setState({ height });
+            const weight = res.data.weight;
+            this.setState({ weight });
+            const name = res.data.name;
+            this.setState({ name });
+          })
+      }
 
     render() {
+        const abilities = [];
+        for (let value of this.state.abilities) {
+            abilities.push(<li key={value}>{value}</li>)
+          }
+        
         return (
-            <p>hello poke details!</p>
+            <React.Fragment>
+                <h2 style={{margin:20}}>{this.state.name}</h2>
+            <ul>
+                <li>Abilities:</li>
+                    <ul>
+                        {abilities}
+                    </ul>
+                <li>Base experience:</li>
+                    <ul>
+                        <li>{this.state.baseExperience}</li>
+                    </ul>
+                <li>Height:</li>
+                    <ul>
+                        <li>{this.state.height}</li>
+                    </ul>
+                <li>Weight:</li>
+                    <ul>
+                        <li>{this.state.weight}</li>
+                    </ul>
+            </ul>
+            </React.Fragment>
         )
-    //     return (
-    //     <React.Fragment>
-    //         <ul>
-    //             { this.state.pokemons.map(pokemon => <li>{pokemon.name}</li>)}
-    //         </ul>
-    //     </React.Fragment>
-    //     );
     }
 }
