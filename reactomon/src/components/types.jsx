9af83e9193e38db39017c0ecb569e4199456bc0d
juditzentai1993 <juditzentai1993@gmail.com>
2020-11-08 01:26:59 +0100

    Component Types return all the pokemon types

--- reactomon/src/components/types.jsx
+++ reactomon/src/components/types.jsx
@@ -1,14 +1,32 @@
 import React, { Component } from "react";
+import axios from 'axios';
 // import "./navbar.css";
 // import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
  
 class Types extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <div>Hello types!</div>
-      </React.Fragment>
-    );
-  }}
+    state = {
+        types: []
+    }
+
+    componentDidMount() {
+        axios.get(`https://pokeapi.co/api/v2/type`)
+          .then(res => {
+            const types = res.data.results;
+            console.log(this.state.types);
+            console.log(types.results);
+            this.setState({ types });
+          })
+      }
+
+    render() {
+        return (
+        <React.Fragment>
+            <ul>
+                { this.state.types.map(pokemon => <li>{pokemon.name}</li>)}
+            </ul>
+        </React.Fragment>
+        );
+    }
+}
  
 export default Types;
