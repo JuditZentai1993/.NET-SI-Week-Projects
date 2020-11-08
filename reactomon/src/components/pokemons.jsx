29dc29dffa231e8cd5b5f299bbd2d5043742c163
juditzentai1993 <juditzentai1993@gmail.com>
2020-11-08 19:52:04 +0100

    Refactor pokemon.jsx componenet, delete unused code freagments, fix slice metod in line 26

--- reactomon/src/components/pokemons.jsx
+++ reactomon/src/components/pokemons.jsx
@@ -1,8 +1,6 @@
 import React, { Component } from "react";
 import axios from 'axios';
 import {NavLink} from 'react-router-dom';
-// import "./navbar.css";
-// import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
  
 export default class Pokemons extends Component {
     state = {
@@ -13,8 +11,6 @@ export default class Pokemons extends Component {
         axios.get(`https://pokeapi.co/api/v2/pokemon?offset=0&limit=1100`)
           .then(res => {
             const pokemons = res.data.results;
-            console.log(this.state.pokemons);
-            console.log(pokemons.results);
             this.setState({ pokemons });
           })
       }
@@ -23,7 +19,7 @@ export default class Pokemons extends Component {
         return (
         <React.Fragment>
             <ul>
-                { this.state.pokemons.map(pokemon => <li><NavLink to={"pokemon/" + pokemon.url.slice(-2)}>{pokemon.name}</NavLink></li>)}
+                { this.state.pokemons.map(pokemon => <li key={pokemon.name}><NavLink to={"pokemon/" + pokemon.url.slice(34,pokemon.url.length)}>{pokemon.name}</NavLink></li>)}
             </ul>
         </React.Fragment>
         );
